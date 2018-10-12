5598f4a789e65ad9c639ae0813b6426519407642
nitin <nitinguptamca@gmail.com>
2018-10-12 18:07:32 +0530

    as

--- src/tests.java
+++ src/tests.java
@@ -1,5 +1,13 @@
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -9,7 +17,40 @@ import java.util.concurrent.Semaphore;
  */
 public class tests {
 
-    public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+		Set<Callable<String>> callables = new HashSet<Callable<String>>();
+
+		callables.add(new Callable<String>() {
+		    public String call() throws Exception {
+		        return "Task 1";
+		    }
+		});
+		callables.add(new Callable<String>() {
+		    public String call() throws Exception {
+		        return "Task 2";
+		    }
+		});
+		callables.add(new Callable<String>() {
+		    public String call() throws Exception {
+		        return "Task 3";
+		    }
+		});
+
+		String result = executorService.invokeAny(callables);
+
+		System.out.println("result = " + result);
+		
+		List<Future<String>> futures = executorService.invokeAll(callables);
+
+		for(Future<String> future : futures){
+		    System.out.println("future.get = " + future.get());
+		}
+
+		executorService.shutdown();
+	}
+    public static void main1(String[] args) throws InterruptedException {
         //creating Random numbers
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
@@ -21,6 +62,16 @@ public class tests {
         sem.release();
         sem.acquire();
         System.out.println("Available Permits " + sem.availablePermits());
+        
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        executorService.execute(new Runnable() {
+            public void run() {
+                System.out.println("Asynchronous task");
+            }
+        });
+
+        executorService.shutdown();
     }
 
 }
